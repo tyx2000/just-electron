b511ddbc6e1ed6ae96bf5f9c862ac851cdd38429
tyxu2000 <tyxu2000@gmail.com>
2025-07-16 17:59:27 +0800

    feat: instant msg

--- renderer.js
+++ renderer.js
@@ -1,8 +1,44 @@
+// const settingButton = document.getElementById("setting-button");
+// settingButton.onclick = () => {
+//   window.api.showMeetingWindow();
+// };
+
+const messages = document.getElementById("messages");
+const inputBox = document.getElementById("inputBox");
+
+const divider = document.getElementById("divider");
+divider.addEventListener("mousedown", (e) => {
+  e.preventDefault();
+  const startY = e.clientY;
+
+  const onMouseMove = (moveEvent) => {
+    const offsetY = moveEvent.clientY - startY;
+    const [messagesHeight, inputBoxHeight] = [
+      getComputedStyle(messages).height,
+      getComputedStyle(inputBox).height,
+    ];
+    console.log("offsetHeight", offsetY, messagesHeight, inputBoxHeight);
+    messages.style.height = `calc(${messagesHeight} + ${offsetY}px)`;
+    inputBox.style.height = `calc(${inputBoxHeight} - ${offsetY}px)`;
+  };
+
+  const onMouseUp = () => {
+    document.removeEventListener("mousemove", onMouseMove);
+    document.removeEventListener("mouseup", onMouseUp);
+  };
+
+  document.addEventListener("mousemove", onMouseMove);
+  document.addEventListener("mouseup", onMouseUp);
+});
+
 const handleSocketMessage = (data) => {
   console.log("socket message", data);
 
   if (data.type === "chat-message") {
     console.log("chat-message", data);
+    const { from, to, timestamp, content } = data;
+    const messageElement = document.createElement("div");
+    messageElement.className = "message";
   } else {
     return;
   }
@@ -27,8 +63,3 @@ window.addEventListener("DOMContentLoaded", async () => {
     console.log("websocketconnection failed", error);
   }
 });
-
-const settingButton = document.getElementById("setting-button");
-settingButton.onclick = () => {
-  window.api.showMeetingWindow();
-};
